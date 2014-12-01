c56e96daabd3d47159e14f5a183495cdc58afedb
Ibrahim Rashid <ibrahim@moulikbd.com>
2014-12-01 22:48:55 +0600

    mandrill template email support added

--- flask_mandrill.py
+++ flask_mandrill.py
@@ -14,6 +14,11 @@ class Mandrill(object):
         self.api_key = app.config['MANDRILL_API_KEY']
         self.app = app
 
+    def _check_config():
+        if not self.api_key:
+            raise ValueError('No Mandrill API key has been configured')
+
+
     def send_email(self, **kwargs):
         """
         Sends an email using Mandrill's API. Returns a
@@ -27,8 +32,8 @@ class Mandrill(object):
         Reference https://mandrillapp.com/api/docs/messages.JSON.html#method=send
         for all the available options.
         """
-        if not self.api_key:
-            raise ValueError('No Mandrill API key has been configured')
+        self._check_config()
+
         data = {
             'async': kwargs.pop('async', False),
             'ip_pool': kwargs.pop('ip_pool', ''),
@@ -42,6 +47,48 @@ class Mandrill(object):
                                  headers={'Content-Type':'application/json'})
         response.raise_for_status()
         return response
+
+    def send_template_email(self, **kwargs):
+        """
+        Sends a template email using Mandrill's API. Returns a
+        Requests :class:`Response` object.
+
+        At a minimum kwargs must contain the keys to, from_email, and text.
+
+        Everything passed as kwargs except for the keywords 'key', 'async',
+        and 'ip_pool' will be sent as key-value pairs in the message object.
+
+        Reference https://mandrillapp.com/api/docs/messages.JSON.html#method=send-template
+        for all the available options.
+        """
+
+        self._check_config()
+
+        if 'template_name' not in kwargs:
+            raise ValueError('Template Name is needed.')
+
+        if 'template_content' not in kwargs:
+            raise ValueError('Template Content is needed')
+
+        data = {
+            'async': kwargs.pop('async', False),
+            'ip_pool': kwargs.pop('ip_pool', ''),
+            'key': kwargs.pop('key', self.api_key),
+            'template_nmae' : kwargs.pop('template_nmae', ''),
+            'template_content' : kwargs.pop('template_content', '')
+            'message': kwargs,
+        }
+        data['message'].setdefault('from_email',
+                                  self.app.config['MANDRILL_DEFAULT_FROM'])
+        response = requests.post(self.messages_endpoint,
+                                 data=json.dumps(data),
+                                 headers={'Content-Type':'application/json'})
+        response.raise_for_status()
+        return response
+
+    @property 
+    def template_messages_endpoint(self):
+        return 'https://mandrillapp.com/api/1.0/messages/send-template.json'
 
     @property
     def messages_endpoint(self):
